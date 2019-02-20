54c8f6f29f01c23c545443695e5efee4e6cff988
Eran Shapira <lxeran@gmail.com>
2019-02-20 22:43:48 +0200

    remove redundant things...

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 // import Image from "../components/image"
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`pitchy`, `application`, `react`]} />
     <h1>Building something awesome!</h1>
-    <Link to="/page-2/">page 2</Link>
+    {/* <Link to="/page-2/">Page 2</Link> */}
   </Layout>
 )
 
