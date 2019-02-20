8d2d2aac3e6c8f867abd88dc80999bab626748cd
Eran Shapira <lxeran@gmail.com>
2019-02-20 22:21:00 +0200

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: `Pitchy`,
+    description: `Tune your piano - the right way.`,
+    author: `@eranshapira`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-catch-links`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `pitchy-site`,
+        short_name: `pitchy`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    'gatsby-plugin-offline',
+  ],
+}
