6c814761fda0477137733d8fb980cd71cd3033d3
Eran Shapira <lxeran@gmail.com>
2019-03-11 20:27:10 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,12 @@ import React from "react"
 import Layout from "../components/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
+// import 'npm-font-open-sans/open-sans';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`pitchy`, `application`, `react`]} />
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet"/>
+<SEO title="Home" keywords={[`pitchy`, `application`, `react`]} />
     <h1>Building something awesome!</h1>
     {/* <Link to="/page-2/">Page 2</Link> */}
   </Layout>
