f33ec42adb1fe2a3e48488f43ab235d6e3e94ca9
Eran Shapira <lxeran@gmail.com>
2019-03-11 20:54:33 +0200

    site after seed

---
+++ data/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteTitle: 'Pitchy', // Site title.
+  siteTitleAlt: 'Pitchy', // Alternative site title for SEO.
+  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
+  siteUrl: 'https://pitchy.netlify.com', // Domain of your website without pathPrefix.
+  pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
+  siteDescription: 'Robotic pitch perfect.', // Website description used for RSS feeds/meta description tag.
+  siteRss: '/rss.xml',
+  siteFBAppID: '', // FB Application ID for using app insights
+  googleTagManagerID: '', // GTM tracking ID.
+  disqusShortname: 'pitchy', // Disqus shortname.
+  userName: 'Eran Shapira',
+  userTwitter: 'lxeran',
+  userLocation: 'Israel',
+  userDescription: '',
+  copyright: 'Copyright © Pitchy 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#e91d63', // Used for setting manifest and progress theme colors.
+  backgroundColor: '#ffffff', // Used for setting manifest background color.
+}
